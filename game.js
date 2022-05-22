53ff8f05f50aa783dbfe32a21b68d977ce176c5e
Stephanelle Rachwan <stephanellerachwan@gmail.com>
2022-05-22 20:40:38 +0300

    Identifying the functions and adding window.onload | JS

---
+++ game.js
@@ -0,0 +1,26 @@
+// The game starts by hovering over S to call the 1st function: startGame
+// We used window onload because the script is in the head section
+window.onload = () => {
+  document.getElementById("start").addEventListener("mouseover", startGame);
+  };
+
+// We will have 5 functions related to the major features
+function startGame() {
+
+}
+
+function winGame() {
+
+}
+
+function loseGame() {
+
+}
+
+function cheatGame() {
+
+}
+
+function resetScore() {
+
+}
