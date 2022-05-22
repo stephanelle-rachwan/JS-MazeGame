1bac2053a088caffa9c6eb6dfaef055e52851254
Stephanelle Rachwan <stephanellerachwan@gmail.com>
2022-05-22 20:44:45 +0300

    Calling the winGame fct and changing the printed instructions | JS

--- game.js
+++ game.js
@@ -5,11 +5,16 @@ window.onload = () => {
   };
 
 // We will have 5 functions related to the major features
+// Function that starts the game and calls other functions based on specific events
 function startGame() {
+  document.getElementById("end").addEventListener("mouseover", winGame)
+  document.getElementById("status").innerHTML = "GAME STARTED: DO NOT TOUCH THE BOUNDARIES | DESTINATION: E";
 
 }
 
+// The win game function that gets executed upon hovering on E
 function winGame() {
+  document.getElementById("status").innerHTML = "You Won!! you get a +5! :)";
 
 }
 
