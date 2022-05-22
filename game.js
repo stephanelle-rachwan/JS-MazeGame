be3d56d397c3310fba85d1214475b3d53ccdbd1f
Stephanelle Rachwan <stephanellerachwan@gmail.com>
2022-05-22 21:09:01 +0300

    Fixing the overlapping boundaries/fct between the boundaries and the end case | JS

--- game.js
+++ game.js
@@ -11,14 +11,20 @@ function startGame() {
   for (let i = 0; i < boundaries.length; i++) {
     boundaries[i].addEventListener("mouseover", loseGame)
   }
+  // removes the boundaries color after hovering over S
+  for (let i = 0; i < boundaries.length; i++) {
+    boundaries[i].classList.remove("youlose");
+  }
   document.getElementById("status").innerHTML = "GAME STARTED: DO NOT TOUCH THE BOUNDARIES | DESTINATION: E";
-
 }
 
 // The win game function that gets executed upon hovering on E
 function winGame() {
   document.getElementById("status").innerHTML = "You Won!! you get a +5! :)";
-
+  // doesn't let the boundaries coloring action execute from E to boundaries
+  for (let i = 0; i < boundaries.length; i++) {
+    boundaries[i].removeEventListener("mouseover", loseGame)
+  }
 }
 
 // The lose game function that gets executed upon crossing of the boundaries
@@ -27,6 +33,8 @@ function loseGame() {
     boundaries[i].classList.add("youlose");
   }
   document.getElementById("status").innerHTML = "You Lost!! You get a -10! :(";
+  // so the winGame fct doesn't execute directly after touching the boundaries then E
+  document.getElementById("end").removeEventListener("mouseover", winGame);
 }
 
 function cheatGame() {
