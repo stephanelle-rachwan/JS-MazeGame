1009190f36ea018f8c244654c022e970d011caf9
Stephanelle Rachwan <stephanellerachwan@gmail.com>
2022-05-22 21:22:15 +0300

    Initializing the score var and implementing the scoring system in the win/lose/reset functions | JS

--- game.js
+++ game.js
@@ -1,10 +1,12 @@
 // The game starts by hovering over S to call the 1st function: startGame
 // We used window onload because the script is in the head section
 window.onload = () => {
+  document.getElementsByClassName('example')[0].innerHTML= "Your score: " + score;
   document.getElementById("start").addEventListener("mouseover", startGame);
   };
 
 const boundaries = document.getElementsByClassName("boundary");
+var score = 0;
 
 // We will have 5 functions related to the major features
 // Function that starts the game and calls other functions based on specific events
@@ -30,6 +32,9 @@ function winGame() {
     boundaries[i].removeEventListener("mouseover", loseGame)
   }
   document.getElementById("game").removeEventListener("mouseleave", cheatGame);
+  score += 5;
+  document.getElementById("end").removeEventListener("mouseover", winGame);
+  document.getElementsByClassName('example')[0].innerHTML= "Your score: " + score;
 }
 
 // The lose game function that gets executed upon crossing of the boundaries
@@ -41,6 +46,11 @@ function loseGame() {
   // so the winGame fct doesn't execute directly after touching the boundaries then E
   document.getElementById("end").removeEventListener("mouseover", winGame);
   document.getElementById("game").removeEventListener("mouseleave", cheatGame);
+  score -= 10;
+  for (let i = 0; i < boundaries.length; i++) {
+    boundaries[i].removeEventListener("mouseover", loseGame)
+  }
+  document.getElementsByClassName('example')[0].innerHTML= "Your score: " + score;
 }
 
 // The cheat game function that gets executed if we try to bypass S from the right
@@ -58,4 +68,6 @@ function cheatGame() {
 // the reset score function, gets executed upon clicking S; resets the score to 0 
 function resetScore() {
   document.getElementById("status").innerHTML = "SCORE RESET | Restart: Get to the E without touching the boundaries";
+  score = 0;
+  document.getElementsByClassName('example')[0].innerHTML= "Your score: " + score;
 }
